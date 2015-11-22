5423905409f2c90bd9ee15c453f3c028e7e63245
bc790 <bc790@york.ac.uk>
2015-11-22 15:23:18 +0000

    Added config

--- desktop/src/com/tealduck/game/desktop/DesktopLauncher.java
+++ desktop/src/com/tealduck/game/desktop/DesktopLauncher.java
@@ -9,6 +9,19 @@ import com.tealduck.game.DuckGame;
 public class DesktopLauncher {
 	public static void main(String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+
+		config.title = "Teal Duck Awesome Game";
+		config.x = -1;
+		config.y = -1;
+		config.width = 600;
+		config.height = 480;
+		config.foregroundFPS = 60;
+
+		// config.resizable = false;
+		// FileType fileType = FileType.Local;
+		// String path = "teal_duck_icon.png";
+		// config.addIcon(path, fileType);
+
 		new LwjglApplication(new DuckGame(), config);
 	}
 }
