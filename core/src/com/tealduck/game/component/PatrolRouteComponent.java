ba3b54f7846d96e3dda3d07c005ddbf88cd355ed
aacn500 <aacn500@users.noreply.github.com>
2015-12-04 17:59:52 +0000

    Rest of patrolling enemies code

---
+++ core/src/com/tealduck/game/component/PatrolRouteComponent.java
@@ -0,0 +1,37 @@
+package com.tealduck.game.component;
+
+import java.util.ArrayList;
+
+import com.badlogic.gdx.math.Vector2;
+import com.tealduck.game.engine.Component;
+
+public class PatrolRouteComponent extends Component {
+	private ArrayList<Vector2> route;
+	private int lastVertex;
+	
+	public PatrolRouteComponent(ArrayList<Vector2> route) {
+		this.route = route;
+		this.lastVertex = 0;
+	}
+	
+	public ArrayList<Vector2> getRoute() {
+		return route;
+	}
+	
+	public Vector2 getNextVertex() {
+		lastVertex = (lastVertex + 1) % route.size();
+		return route.get(lastVertex);
+	}
+	
+	public Vector2 getTarget() {
+		return route.get(lastVertex);
+	}
+	
+	public Vector2 getElementByIndex(int index) {
+		return route.get(index);
+	}
+	
+	public String toString() {
+		return route.toString();
+	}
+}
