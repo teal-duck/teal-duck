eb633710d1ac49d4e8774292d40d5ec47a7fce80
aacn500 <aacn500@users.noreply.github.com>
2016-01-17 19:02:01 +0000

    Added EntityEngineTest

---
+++ core/src/com/tealduck/game/tests/engine/EntityEngineTest.java
@@ -0,0 +1,27 @@
+package com.tealduck.game.tests.engine;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.tealduck.game.engine.EntityEngine;
+
+public class EntityEngineTest {
+	private EntityEngine entityEngine;
+	
+	@Before
+	public void setup() {
+		entityEngine = new EntityEngine();
+	}
+	
+	@Test
+	public void testRemoveEntities() {
+		int entity1 = entityEngine.getEntityManager().createEntity();
+		
+		Assert.assertTrue(entityEngine.getEntityManager().getEntities().contains(entity1));
+		entityEngine.flagEntityToRemove(entity1);
+		entityEngine.removeAllFlaggedEntities();
+		
+		Assert.assertFalse(entityEngine.getEntityManager().getEntities().contains(entity1));
+	}
+}
