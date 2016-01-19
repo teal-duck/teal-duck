5a36d02b47375856be3857c31d947c798c7f3fbe
aacn500 <aacn500@users.noreply.github.com>
2016-01-19 20:58:17 +0000

    Removed superfluous test

--- core/src/com/tealduck/game/tests/AllTests.java
+++ core/src/com/tealduck/game/tests/AllTests.java
@@ -21,15 +21,14 @@ import com.tealduck.game.tests.engine.EventManagerTest;
 import com.tealduck.game.tests.engine.SystemManagerTest;
 import com.tealduck.game.tests.pickup.AmmoPickupTest;
 import com.tealduck.game.tests.pickup.HealthPickupTest;
-import com.tealduck.game.tests.system.ChaseSystemTest;
 
 
 @RunWith(Suite.class)
 @SuiteClasses({ AABBTest.class, CircleTest.class, CollisionTest.class, IntersectionTest.class,
 		PatrolRouteComponentTest.class, PositionComponentTest.class, ScoreComponentTest.class,
 		WeaponComponentTest.class, EntityEngineTest.class, EntityManagerTest.class, EntityTagManagerTest.class,
-		EventManagerTest.class, SystemManagerTest.class, AmmoPickupTest.class, HealthPickupTest.class,
-		ChaseSystemTest.class })
+		EventManagerTest.class, SystemManagerTest.class, AmmoPickupTest.class, HealthPickupTest.class
+		})
 
 public class AllTests {
 
